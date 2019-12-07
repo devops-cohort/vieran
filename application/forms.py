6c04d158048188d26ef865a919b119855a92993d
TheBartThe <vieran96@gmail.com>
2019-12-07 14:42:00 +0000

    added view teams route

--- application/forms.py
+++ application/forms.py
@@ -113,132 +113,132 @@ class UpdateAccountForm(FlaskForm):
             if user:
                 raise ValidationError('This email is already in use - please choose another')
 
-##gkdata = Player.query.filter_by(position="GK").all()
-##defdata = Player.query.filter_by(position="DEF").all()
-##middata = Player.query.filter_by(position="MID").all()
-##fwddata = Player.query.filter_by(position="FWD").all()
+gkdata = Player.query.filter_by(position="GK").all()
+defdata = Player.query.filter_by(position="DEF").all()
+middata = Player.query.filter_by(position="MID").all()
+fwddata = Player.query.filter_by(position="FWD").all()
+
+gk_choices=[]
+def_choices=[]
+mid_choices=[]
+fwd_choices=[]
+
+for i in range(len(gkdata)):
+    gk_choices.append([gkdata[i].last_name, gkdata[i].last_name + ": " + gkdata[i].club])
+
+for i in range(len(defdata)):
+    def_choices.append([defdata[i].last_name, defdata[i].last_name + ": " + defdata[i].club])
+
+for i in range(len(middata)):
+    mid_choices.append([middata[i].last_name, middata[i].last_name + ": " + middata[i].club])
+
+for i in range(len(fwddata)):
+    fwd_choices.append([fwddata[i].last_name, fwddata[i].last_name + ": " + fwddata[i].club])
+
+
+## Comment out createteamform when creating databases
+class CreateTeamForm(FlaskForm):
+
+    team_name = StringField('Team name',
+        validators=[
+            DataRequired(),
+            Length(min=2, max=25)
+        ]
+    )
+    
+    goalkeeper = SelectField('Goalkeeper',
+        choices=gk_choices,
+        validators=[
+            DataRequired()
+        ]
+    )
+
+    defence = SelectField("Defender",
+        choices=def_choices,
+        validators=[
+            DataRequired()
+        ]
+    )
+
+    midfield = SelectField("Midfielder",
+        choices=mid_choices,
+        validators=[
+            DataRequired()
+        ]
+    )
+
+    attack = SelectField('Striker',
+        choices=fwd_choices,
+        validators=[
+            DataRequired()
+        ]
+    )
+
+    submit = SubmitField('Create team')
+
+    def validate_team_name(self, team_name):
+        team = Team.query.filter_by(team_name=team_name.data).first()
+        if team:
+            raise ValidationError('Someone already has that team name, try a different one!')
+
+# This was needed for SelectMultipleForm, didn't use it but may change
+##    def validate_gk(self, goalkeeper):
+##        if (len(self.goalkeeper.data) != 1):
+##            raise ValidationError('You must select one goalkeeper!')
 ##
-##gk_choices=[]
-##def_choices=[]
-##mid_choices=[]
-##fwd_choices=[]
+##    def validate_def(self, defence):
+##        if (len(self.defence.data) != 1):
+##            raise ValidationError('You must select one defender!')
 ##
-##for i in range(len(gkdata)):
-##    gk_choices.append([gkdata[i].last_name, gkdata[i].last_name + ": " + gkdata[i].club])
+##    def validate_mid(self, midfield):
+##        if (len(self.midfield.data) != 2):
+##            raise ValidationError('You must select two midfielders!')
 ##
-##for i in range(len(defdata)):
-##    def_choices.append([defdata[i].last_name, defdata[i].last_name + ": " + defdata[i].club])
-##
-##for i in range(len(middata)):
-##    mid_choices.append([middata[i].last_name, middata[i].last_name + ": " + middata[i].club])
-##
-##for i in range(len(fwddata)):
-##    fwd_choices.append([fwddata[i].last_name, fwddata[i].last_name + ": " + fwddata[i].club])
-##
-##
-#### Comment out createteamform when creating databases
-##class CreateTeamForm(FlaskForm):
-##
-##    team_name = StringField('Team name',
-##        validators=[
-##            DataRequired(),
-##            Length(min=2, max=25)
-##        ]
-##    )
-##    
-##    goalkeeper = SelectField('Goalkeeper',
-##        choices=gk_choices,
-##        validators=[
-##            DataRequired()
-##        ]
-##    )
-##
-##    defence = SelectField("Defender",
-##        choices=def_choices,
-##        validators=[
-##            DataRequired()
-##        ]
-##    )
-##
-##    midfield = SelectField("Midfielder",
-##        choices=mid_choices,
-##        validators=[
-##            DataRequired()
-##        ]
-##    )
-##
-##    attack = SelectField('Striker',
-##        choices=fwd_choices,
-##        validators=[
-##            DataRequired()
-##        ]
-##    )
-##
-##    submit = SubmitField('Create team')
-##
-##    def validate_team_name(self, team_name):
-##        team = Team.query.filter_by(team_name=team_name.data).first()
-##        if team:
-##            raise ValidationError('Someone already has that team name, try a different one!')
-##
-### This was needed for SelectMultipleForm, didn't use it but may change
-####    def validate_gk(self, goalkeeper):
-####        if (len(self.goalkeeper.data) != 1):
-####            raise ValidationError('You must select one goalkeeper!')
-####
-####    def validate_def(self, defence):
-####        if (len(self.defence.data) != 1):
-####            raise ValidationError('You must select one defender!')
-####
-####    def validate_mid(self, midfield):
-####        if (len(self.midfield.data) != 2):
-####            raise ValidationError('You must select two midfielders!')
-####
-####    def validate_fwd(self, attack):
-####        if (len(self.attack.data) != 1):
-####            raise ValidationError('You must select one striker!')
-##
-##class TransferForm(FlaskForm):
-##
-##    team_name = StringField('Team name',
-##        validators=[
-##            DataRequired(),
-##            Length(min=2, max=25)
-##        ]
-##    )
-##    
-##    goalkeeper = SelectField('Goalkeeper',
-##        choices=gk_choices,
-##        validators=[
-##            DataRequired()
-##        ]
-##    )
-##
-##    defence = SelectField("Defender",
-##        choices=def_choices,
-##        validators=[
-##            DataRequired()
-##        ]
-##    )
-##
-##    midfield = SelectField("Midfielder",
-##        choices=mid_choices,
-##        validators=[
-##            DataRequired()
-##        ]
-##    )
-##
-##    attack = SelectField('Striker',
-##        choices=fwd_choices,
-##        validators=[
-##            DataRequired()
-##        ]
-##    )
-##
-##    submit = SubmitField('Create team')
-##
-##    def validate_team_name(self, team_name):
-##        if team_name.data != current_user.team_name:
-##            user = Users.query.filter_by(team_name=team_name.data).first()
-##            if user:
-##                raise ValidationError('This team name is already in use - please choose another')
+##    def validate_fwd(self, attack):
+##        if (len(self.attack.data) != 1):
+##            raise ValidationError('You must select one striker!')
+
+class TransferForm(FlaskForm):
+
+    team_name = StringField('Team name',
+        validators=[
+            DataRequired(),
+            Length(min=2, max=25)
+        ]
+    )
+    
+    goalkeeper = SelectField('Goalkeeper',
+        choices=gk_choices,
+        validators=[
+            DataRequired()
+        ]
+    )
+
+    defence = SelectField("Defender",
+        choices=def_choices,
+        validators=[
+            DataRequired()
+        ]
+    )
+
+    midfield = SelectField("Midfielder",
+        choices=mid_choices,
+        validators=[
+            DataRequired()
+        ]
+    )
+
+    attack = SelectField('Striker',
+        choices=fwd_choices,
+        validators=[
+            DataRequired()
+        ]
+    )
+
+    submit = SubmitField('Create team')
+
+    def validate_team_name(self, team_name):
+        if team_name.data != current_user.team_name:
+            user = Users.query.filter_by(team_name=team_name.data).first()
+            if user:
+                raise ValidationError('This team name is already in use - please choose another')
